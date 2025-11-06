9f5f9d7c454e36c93f46a28598d531e3d461c4b0
gd0x <df.gustavofranco@gmail.com>
2025-11-06 14:16:01 -0300

    teste

--- src/Main.java
+++ src/Main.java
@@ -96,6 +96,30 @@ public class Main {
         }
         */
 
+        /*
+        Scanner sc = new Scanner(System.in);
+
+        double num = sc.nextDouble();
+
+        if (num >= 0 && num <= 25){
+            System.out.print("Intervalo [0,25]");
+        }
+        else if (num > 25 && num <= 50) {
+            System.out.print("Intervalo (25,50]");
+        }
+        else if (num > 50 && num <= 75){
+            System.out.print("Intervalo (50,75]");
+        }
+        else if (num > 75 && num <= 100){
+            System.out.print("Intervalo (75,100]");
+        }
+        else {
+            System.out.print("Fora de intervalo");
+        }
+        */
+
+        
+
     }
 
 }
