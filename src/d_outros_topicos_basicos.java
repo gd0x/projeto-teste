3d28e6cd367f616110c14ca04c4bea4916d97b77
gd0x <df.gustavofranco@gmail.com>
2025-11-07 19:04:20 -0300

    new file

---
+++ src/d_outros_topicos_basicos.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class d_outros_topicos_basicos {
+
+    public static void main (String[] args){
+
+        //Operadores bitwise
+        /*
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = 89;
+        int n2 = 60;
+
+        System.out.println(n1 & n2); // E bit a bit
+        System.out.println(n1 | n2); // ou bit a bit
+        System.out.println(n1 ^ n2); // ou exclusivo bit a bit
+
+        int mask = 0b100000;
+        int n = sc.nextInt();
+
+        if ((n & mask) != 0){
+            System.out.println("6th bit is true!");
+        }else {
+            System.out.println("6th bit is false!");
+        }
+        */
+
+        //Funções interessantes para Strings (para consulta/decorar com o tempo)
+        /*
+        String original = "abcde FGHIJ ABC abc DEFG    ";
+
+        String s01 = original.toLowerCase();
+        String s02 = original.toUpperCase();
+        String s03 = original.trim();
+        String s04 = original.substring(2);
+        String s05 = original.substring(2, 9); //c e I
+        String s06 = original.replace('a', 'x');
+        String s07 = original.replace("abc", "xy");
+        int i = original.indexOf("bc");
+        int j = original.lastIndexOf("bc");
+
+        System.out.println("Original: -" + original + "-");
+        System.out.println("toLowerCase: -" + s01 + "-");
+        System.out.println("toUpperCase: -" + s02 + "-");
+        System.out.println("trim: -" + s03 + "-");
+        System.out.println("substring(2): -" + s04 + "-");
+        System.out.println("substring(2, 9): -" + s05 + "-");
+        System.out.println("replace('a', 'x'): -" + s06 + "-");
+        System.out.println("replace('abc', 'xy'): -" + s07 + "-");
+        System.out.println("Index of 'bc': " + i);
+        System.out.println("lastIndex of 'bc': " + j);
+
+        String s = "potato apple lemon";
+
+        String[] vect = s.split(" ");
+        String word1 = vect[0];
+        String word2 = vect[1];
+        String word3 = vect[2];
+
+        System.out.print(word1 + " " + word2 + " " + " " + word3);
+        */
+
+        
+
+    }
+
+}
