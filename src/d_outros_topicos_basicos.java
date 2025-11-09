f5a03bb4d4ede7a7e5ff6d0ac2bc694285d66ec0
gd0x <df.gustavofranco@gmail.com>
2025-11-09 09:37:06 -0300

    funcoes_sintaxe

--- src/d_outros_topicos_basicos.java
+++ src/d_outros_topicos_basicos.java
@@ -60,6 +60,34 @@ public class d_outros_topicos_basicos {
         System.out.print(word1 + " " + word2 + " " + " " + word3);
         */
 
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int higher = max(a, b, c);
+        showResult(higher);
+
+    }
+
+    public static int max(int x, int y, int z) {
+        int aux;
+        if (x > y && x > z) {
+            aux = x;
+        } else if (y > z) {
+            aux = y;
+        } else {
+            aux = z;
+        }
+
+        return aux;
+
+    }
+
+    public static void showResult(int value) {
+
+        System.out.println("Higher = " + value);
 
     }
 
