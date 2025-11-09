ff416d7b58f8060b6ff9680295a3b541338caafc
gd0x <df.gustavofranco@gmail.com>
2025-11-09 15:19:33 -0300

    Exercicio

--- OOP Classes/src/entities/Product.java
+++ OOP Classes/src/entities/Product.java
@@ -2,10 +2,31 @@ package entities;
 
 public class Product {
 
-    public static void main (String[] args){
+    public String name;
+    public double price;
+    public int quantity;
 
+    public double totalValueInStock() {
+        return price * quantity;
+    }
 
+    public void addProduct(int quantity) {
+        this.quantity += quantity;
+    }
+
+    public void removeProduct(int quantity) {
+        this.quantity -= quantity;
+    }
+    public String toString(){
+        return name
+            + ", $ "
+            + String.format("%.2f", price)
+            + ", "
+            + quantity
+            + " units, Total: $ "
+            + String.format("%.2f", totalValueInStock());
 
     }
+
 
 }
